ef645a33305df45c172eb9af993c9e7ffea527e4
sushmita-2001 <82380401+sushmita-2001@users.noreply.github.com>
2021-05-29 13:31:49 +0530

    Add files via upload

---
+++ checkno.py
@@ -0,0 +1,9 @@
+#check whether a number is in between 1 and 10
+
+num = int(input("Enter the no. you want to check\n"))
+
+if num >= 1 and num <= 10 :
+    print("No. is between 1 and 10")
+
+else:
+    print("No. is not between 1 and 10")
---
+++ digit.py
@@ -0,0 +1,19 @@
+# WAP to accept a numeric digit and display it in words.
+
+num = int(input("Enter a digit\n"))
+
+if num== 0 :  print("zero")
+elif num == 1 : print("one")
+elif num == 2 : print("two")
+elif num == 3 : print("Three")
+elif num == 4 : print("four")
+elif num == 5 : print("five")
+elif num == 6 : print("six")
+elif num == 7 : print("seven")
+elif num == 8 : print("eight")
+elif num == 9 : print("nine")
+else : print("am still learning")
+
+
+
+
