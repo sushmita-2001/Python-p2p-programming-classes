c7453a907823253400e357f8de74b5d913dec276
Sushmita <82380401+sushmita-2001@users.noreply.github.com>
2021-05-30 16:21:53 +0530

    Add files via upload

---
+++ celcius.py
@@ -0,0 +1,4 @@
+
+c =int (input ("enter temprature :"))
+F= c*(9/5) + 32
+print("temprature in farenhite", F)
---
+++ kilomiles.py
@@ -0,0 +1,6 @@
+# 1 kilometer = 0.6214 miles 
+
+km = int(input("How much kilometers you want to convert?\n"))
+
+miles = (km*0.6214)
+print(miles)
